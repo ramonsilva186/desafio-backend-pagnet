3bcf53808ed7ab171db3a3877044ee9ffa244202
ramonsilva186 <ramonsilva186@gmail.com>
2024-03-18 22:34:04 -0300

    batch initial config

---
+++ src/main/java/br/com/ramonsilva/backend/job/BatchConfig.java
@@ -0,0 +1,46 @@
+package br.com.ramonsilva.backend.job;
+
+import br.com.ramonsilva.backend.entity.Transacao;
+import br.com.ramonsilva.backend.entity.TransacaoCNAB;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class BatchConfig {
+    private PlatformTransactionManager transactionManager;
+    private JobRepository jobRepository;
+
+    public BatchConfig(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+    /*
+    @Bean
+    Job job(Step step) {
+        return new JobBuilder("job", jobRepository)
+                .start(step)
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+
+    @Bean
+    Step step(ItemReader<TransacaoCNAB> reader, ItemProcessor<TransacaoCNAB, Transacao> processor, ItemWriter<Transacao> writer) {
+        return new StepBuilder("step", jobRepository)
+                .<TransacaoCNAB, Transacao>chunk(1000, transactionManager)
+                .reader(reader)
+                .processor(processor)
+                .writer(writer)
+                .build();
+    }
+    */
+
+}
