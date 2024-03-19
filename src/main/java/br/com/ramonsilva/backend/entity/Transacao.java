e190123d059a7a954114f01280ff78d81d638711
ramonsilva186 <ramonsilva186@gmail.com>
2024-03-19 13:30:45 -0300

    batch config completed

--- src/main/java/br/com/ramonsilva/backend/entity/Transacao.java
+++ src/main/java/br/com/ramonsilva/backend/entity/Transacao.java
@@ -2,6 +2,8 @@ package br.com.ramonsilva.backend.entity;
 
 import java.math.BigDecimal;
 import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public record Transacao(Long id,
@@ -13,4 +15,24 @@ public record Transacao(Long id,
                         Time hora,
                         String donoDaLoja,
                         String nomeDaLoja) {
+
+
+    public Transacao withValor(BigDecimal valor) {
+        return new Transacao(this.id, this.tipo, this.data, valor, this.cpf, this.cartao, this.hora, this.donoDaLoja, this.nomeDaLoja);
+    }
+
+    public Transacao withData(String data) throws ParseException {
+        var dateFormat = new SimpleDateFormat("yyyyMMdd");
+        var date = dateFormat.parse(data);
+
+        return new Transacao(this.id, this.tipo, new Date(date.getTime()), this.valor, this.cpf, this.cartao, this.hora, this.donoDaLoja, this.nomeDaLoja);
+
+    }
+
+    public Transacao withHora(String hora) throws ParseException {
+        var timeFormat = new SimpleDateFormat("HHmmss");
+        var time = timeFormat.parse(hora);
+
+        return new Transacao(this.id, this.tipo, this.data, this.valor, this.cpf, this.cartao, new Time(time.getTime()), this.donoDaLoja, this.nomeDaLoja);
+    }
 }
