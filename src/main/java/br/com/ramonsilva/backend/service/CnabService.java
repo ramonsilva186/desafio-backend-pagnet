b916690453e1e0a81fb3520b94b335919f5685fe
ramonsilva186 <ramonsilva186@gmail.com>
2024-03-19 22:12:37 -0300

    create upload cnab file

---
+++ src/main/java/br/com/ramonsilva/backend/service/CnabService.java
@@ -0,0 +1,24 @@
+package br.com.ramonsilva.backend.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+public class CnabService {
+
+    private final Path fileStorageLocation;
+
+    public CnabService(@Value("${file.upload-dir}") String fileUploadDir) {
+        this.fileStorageLocation = Paths.get(fileUploadDir);
+    }
+    public void uploadCnabFile(MultipartFile file) throws Exception {
+        var fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        var targetLocation = fileStorageLocation.resolve(fileName);
+        file.transferTo(targetLocation);
+    }
+}
