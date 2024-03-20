e037e580d4edc139bbf9e6c921d81cca7561b77c
ramonsilva186 <ramonsilva186@gmail.com>
2024-03-19 21:29:38 -0300

    refact Batch config

--- src/main/java/br/com/ramonsilva/backend/job/BatchConfig.java
+++ src/main/java/br/com/ramonsilva/backend/job/BatchConfig.java
@@ -79,9 +79,8 @@ public class BatchConfig {
         return item -> {
             var transacao = new Transacao(
                     null, item.tipo(), null,
-                    null, item.cpf(), item.cartao(),
+                    item.valor().divide(BigDecimal.valueOf(100)), item.cpf(), item.cartao(),
                     null, item.donoDaLoja().trim(), item.nomeDaLoja().trim())
-                    .withValor(item.valor().divide(BigDecimal.valueOf(100)))
                     .withData(item.data())
                     .withHora(item.hora());
             return transacao;
