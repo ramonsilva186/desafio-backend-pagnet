979a0485647f52ce53fb6566db2bd184ce3dd6f3
ramonsilva186 <ramonsilva186@gmail.com>
2024-03-21 15:54:53 -0300

    find if credit ou debit

---
+++ src/main/java/br/com/ramonsilva/backend/entity/TipoTransacao.java
@@ -0,0 +1,37 @@
+package br.com.ramonsilva.backend.entity;
+
+import java.math.BigDecimal;
+
+public enum TipoTransacao {
+    DEBITO(1),
+    BOLETO(2),
+    FINANCIAMENTO(3),
+    CREDITO(4),
+    EMPRESTIMO(5),
+    VENDAS(6),
+    RECEBIMENTO_TED(7),
+    RECEBIMENTO_DOC(8),
+    ALUGUEL(9);
+    private int tipo;
+
+    private TipoTransacao(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public BigDecimal getSinal() {
+        return switch (tipo) {
+            case 1, 4, 5, 6, 7, 8 -> new BigDecimal(1);
+            case 2, 3, 9 -> new BigDecimal(-1);
+            default -> new BigDecimal(0);
+        };
+    }
+
+    public static TipoTransacao findByTipo(int tipo) {
+        for (TipoTransacao tipoTransacao : values()) {
+            if (tipoTransacao.tipo == tipo) {
+                return tipoTransacao;
+            }
+        }
+        throw new IllegalArgumentException("Tipo de transação inválido: " + tipo);
+    }
+}
--- src/main/java/br/com/ramonsilva/backend/service/TransacaoService.java
+++ src/main/java/br/com/ramonsilva/backend/service/TransacaoService.java
@@ -1,5 +1,6 @@
 package br.com.ramonsilva.backend.service;
 
+import br.com.ramonsilva.backend.entity.TipoTransacao;
 import br.com.ramonsilva.backend.entity.Transacao;
 import br.com.ramonsilva.backend.entity.TransacaoReport;
 import br.com.ramonsilva.backend.repository.TransacaoRepository;
@@ -25,11 +26,14 @@ public class TransacaoService {
         var reportMap = new LinkedHashMap<String, TransacaoReport>();
         transacoes.forEach(transacao -> {
             String nomeDaLoja = transacao.nomeDaLoja();
-            BigDecimal valor = transacao.valor();
+            var tipoTransacao = TipoTransacao.findByTipo(transacao.tipo());
+
+            BigDecimal valor = transacao.valor().multiply(tipoTransacao.getSinal());
 
             reportMap.compute(nomeDaLoja, (key, existingReport) -> {
                 var report = (existingReport != null) ? existingReport: new TransacaoReport(key, BigDecimal.ZERO, new ArrayList<>());
-                return report.addTotal(valor).addTransacao(transacao);
+
+                return report.addTotal(valor).addTransacao(transacao.withValor(valor));
             });
         });
 
